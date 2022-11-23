00b24eda016d355a3ea7ad0815c9449e2c489b16
Darko Zlatarek <darko.zlatarek@gmail.com>
2022-11-23 22:50:53 +0000

    add keyboard into HTML with javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,3 +42,38 @@ var animals = [
     "wolf",
     "panda"
 ]
+
+
+const keys = [
+    "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+    "a", "s", "d", "f", "g", "h", "j", "k", "l",
+    "z", "x", "c", "v", "b", "n", "m"
+];
+
+
+let answer = "";
+let guessed = [];
+const keyboard = document.querySelector("#key-container");
+
+/**
+ * Will generate a random animal from the animals array
+ * for the user tu guess
+ */
+function randomAnimal() {
+    answer = animals[Math.floor(Math.random() * animals.length)];
+}
+
+keys.forEach(key => {
+    const buttonElement = document.createElement("button");
+    const insertLineBreak = ["p", "l"].indexOf(key) !== -1;
+    buttonElement.textContent = key;
+    buttonElement.setAttribute("id", key);
+    keyboard.appendChild(buttonElement);
+
+
+    if (insertLineBreak) {
+        buttonElement.nextSibling(document.createElement("br"));
+    }
+});
+
+randomAnimal();
